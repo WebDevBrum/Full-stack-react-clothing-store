1d76e98cf23110751371a72c5a1f0900e8d0cf1c
Scott Davies <scottdavies1983@gmail.com>
2020-10-13 19:27:46 +0100

    items creation and prisma yoga flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,14 @@
 const Mutations = {
-  createItem(parent, args, ctx, info) {
+  async createItem(parent, args, ctx, info) {
     //TODO: check if they are logged in
 
-    const item = ctx.db.mutation.createItem({
+    const item = await ctx.db.mutation.createItem({
       data: {
         ...args
       }
     }, info);
+
+    return item;
   }
   // createDog(parent, args, ctx, info) {
   //   //create a dog
