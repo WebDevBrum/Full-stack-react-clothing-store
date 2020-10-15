ad446804c8589597f1bad1316d489fdd6768cef4
Scott Davies <scottdavies1983@gmail.com>
2020-10-15 19:49:01 +0100

    initial apollo setup

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bf734404794b0e74095";
+/******/ 	var hotCurrentHash = "63827a5535182f1aa151";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
